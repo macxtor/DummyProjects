2a35b3a06954c0c9dd41c07e19b2db1bed691bdf
Macxtor <macxtor04@gmail.com>
2023-01-19 20:26:32 +0000

    add usecase with unit test

---
+++ app/src/main/java/com/macxtor/dummy_products/domain/usecase/GetAllProductsUseCase.kt
@@ -0,0 +1,12 @@
+package com.macxtor.dummy_products.domain.usecase
+
+import com.macxtor.dummy_products.domain.repository.ProductsRepository
+import javax.inject.Inject
+
+class GetAllProductsUseCase @Inject constructor(
+    private val productsRepository: ProductsRepository
+) {
+
+    suspend fun execute() = productsRepository.getAllProducts()
+
+}
